bf5c9dad49dae1e372c9c282852d2d62b6fc524d
DevR-code <mydevplace@outlook.com>
2024-09-19 00:40:40 +0100

    added .htaccess app and index.php

---
+++ app/core/Config.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Core;
+
+class Config{
+    private static $config = [
+        "version" => '0.0.1',
+        "root_dir" => '/cms/',  // "/" on linve server
+        "default_controller" => 'Blog',         //default home ctrler
+        "default_layout" => 'default',      //default ;layout
+        "default_site_title" => 'Freewriter',   //default site title
+        "db_host" => '127.0.0.1',     //DB host ---faster with IP address
+        "db_name" => 'cms',           //DB Name
+        "db_user" => 'root',         //DB user
+        "db_password" => '',       //DB password
+        "login_cookie_name" => 'm#lc#nm'       //login cookie name
+    ];
+
+    public static function get(){
+        return array_key_exists($key, self::$config) ? self::$config[$key] : NULL;
+    }
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+
+//define constant
+define('PROOT', __DIR__);
+define('DS', DIRECTORY_SEPARATOR);  //   "/"
+//var_dump(PROOT);
+//var_dump(DS);
+
+
